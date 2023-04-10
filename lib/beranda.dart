a108ef59da7ed4f7cd84b55666b94b4b4c9c82d0
Alfian <alfianm@fnbees.com>
2023-04-10 22:43:33 +0700

    add listview rest api

--- lib/beranda.dart
+++ lib/beranda.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:coba_alterra/todo_list_provider.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -17,53 +20,101 @@ class BerandaPage extends StatefulWidget {
 }
 
 class _BerandaPageState extends State<BerandaPage> {
+  List<String> todo = [];
+  List<bool> completed = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTodos();
+  }
+
+  getTodos() async {
+    final dio = Dio();
+    final Response response = await dio.get(
+      'https://dummyjson.com/todos',
+      options: Options(
+        validateStatus: (_) => true,
+      ),
+    );
+    if(response.statusCode == 200){
+      response.data['todos'].forEach((element) {
+        setState(() {
+          todo.add(element['todo']);
+          completed.add(element['completed']);
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text('Hi, ${widget.firstName} ${widget.lastName}'),
         Expanded(
-          child: Consumer<TodoListProvider>(
-            builder: (BuildContext context, TodoListProvider provider, Widget? child) {
-              return ListView.builder(
-                itemCount: provider.todoList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            provider.todoList[index]['name'],
-                            style: TextStyle(
-                              color: provider.todoList[index]['status'] ? Colors.grey : null,
-                              decoration: provider.todoList[index]['status'] ? TextDecoration.lineThrough : null,
-                            ),
-                          ),
-                          // Checkbox(
-                          //   value: todoListProvider.todoList[index]['status'],
-                          //   onChanged: (bool? value) {
-                          //     setState(() {
-                          //       todoListProvider.todoList[index]['status'] = value!;
-                          //     });
-                          //   },
-                          // ),
-                          IconButton(
-                            onPressed: () {
-                              provider.deleteTodo(index);
-                            },
-                            icon: const Icon(Icons.delete),
-                          )
-                        ],
-                      ),
+          child: ListView.builder(
+            itemCount: todo.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    todo[index],
+                    style: TextStyle(
+                      overflow: TextOverflow.ellipsis,
+                      color: completed[index] ? Colors.grey : null,
+                      decoration: completed[index] ? TextDecoration.lineThrough : null,
                     ),
-                  );
-                },
+                  ),
+                ),
               );
             },
           ),
         ),
+        // Expanded(
+        //   child: Consumer<TodoListProvider>(
+        //     builder: (BuildContext context, TodoListProvider provider, Widget? child) {
+        //       return ListView.builder(
+        //         itemCount: provider.todoList.length,
+        //         itemBuilder: (BuildContext context, int index) {
+        //           return Card(
+        //             child: Padding(
+        //               padding: const EdgeInsets.all(16),
+        //               child: Row(
+        //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //                 children: [
+        //                   Text(
+        //                     provider.todoList[index]['name'],
+        //                     style: TextStyle(
+        //                       color: provider.todoList[index]['status'] ? Colors.grey : null,
+        //                       decoration: provider.todoList[index]['status'] ? TextDecoration.lineThrough : null,
+        //                     ),
+        //                   ),
+        //                   // Checkbox(
+        //                   //   value: todoListProvider.todoList[index]['status'],
+        //                   //   onChanged: (bool? value) {
+        //                   //     setState(() {
+        //                   //       todoListProvider.todoList[index]['status'] = value!;
+        //                   //     });
+        //                   //   },
+        //                   // ),
+        //                   IconButton(
+        //                     onPressed: () {
+        //                       provider.deleteTodo(index);
+        //                     },
+        //                     icon: const Icon(Icons.delete),
+        //                   )
+        //                 ],
+        //               ),
+        //             ),
+        //           );
+        //         },
+        //       );
+        //     },
+        //   ),
+        // ),
       ],
     );
   }
